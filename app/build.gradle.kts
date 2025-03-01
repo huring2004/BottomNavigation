6a218abcfcce2c9714cf6a0c7e9e1c0a116213c3
QuangHung <quanghungbg2021@gmail.com>
2025-03-01 22:23:40 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.excerise.bottomnavigation"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.excerise.bottomnavigation"
